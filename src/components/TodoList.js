aa0220ec2885a5614c7b47563a21c2583551995b
sree <minnualankar@gmail.com>
2023-06-06 22:05:26 +0530

    removed checkbox

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -5,7 +5,6 @@ const TodoList = ({ todos, handleDelete, handleEdit, handleCheck }) => {
     <ul className="allTodos">
       {todos.map((t) => (
         <li className="singleTodo">
-          <input type="checkbox" style={{width:"20px"}} onChange={()=> handleCheck(t.id)}></input>
           <span id="{t.id}" className="todoText" key={t.id} >
             {t.todo}
           </span>
